d88705cf7c89542ef07186f98caf47db3d7982e4
dmitry <dmitry.zyryanov@execution.su>
2019-03-04 18:41:56 +0700

    show if user is logged in

--- airhorn/app/index.js
+++ airhorn/app/index.js
@@ -17,24 +17,56 @@ function getUsers() {
   });
 }
 
+function loginUser(login, password) {
+  console.log('loginuser', login, password);
+  return getUsers()
+      .then(users => {
+        const user = users.find(user => user.username === login);
+        console.log(111, user);
+        if (!user) {
+          return 404;
+        }
+        if (user.password !== password) {
+          return 401;
+        }
+        return user;
+      });
+}
+
 app.get(
   '/api/login',
   (req, res) => {
-    getUsers()
-        .then(users => {
-          const {login, password} = req.query;
-          const user = users.find(user => user.username === login);
-          if (!user) {
-            res.status(404).end();
-            return;
-          }
-          if (user.password !== password) {
-            res.status(401).end();
+    const {login, password} = req.query;
+    loginUser(login, password)
+        .then(userOrStatus => {
+          if (typeof userOrStatus !== 'object') {
+            res.status(userOrStatus).end();
             return;
           }
           res.status(200).end(`${login}\n${password}\n${+new Date()}`);
         });
   },
+);
+
+app.get(
+  '/api/userinfo',
+  (req, res) => {
+    const {token} = req.query;
+    const [login, password, date] = token.split('\n');
+    if (new Date() - date > 30000) {
+      console.log('date expired', date, new Date(), new Date() - date);
+      res.status(401).end();
+      return;
+    }
+    loginUser(login, password)
+        .then(userOrStatus => {
+          if (typeof userOrStatus !== 'object') {
+            res.status(userOrStatus).end();
+            return;
+          }
+          res.status(200).json(userOrStatus);
+        });
+  }
 );
 
 app.listen(8002, () => {
