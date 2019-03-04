73de07e4aeb81aa0ba0f53d7290a321e24a72729
dmitry <dmitry.zyryanov@execution.su>
2019-03-04 17:31:26 +0700

    get users list and login API

--- airhorn/app/index.js
+++ airhorn/app/index.js
@@ -1,7 +1,53 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
 app.use(express.static('public'));
+
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./users.json', 'utf8', (err, text) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(JSON.parse(text));
+    });
+  });
+}
+
+app.get(
+  '/users',
+  (req, res) => {
+    getUsers()
+        .then(users => {
+          console.log('users=', users);
+          res.json(users);
+          res.end();
+        });
+  },
+);
+
+app.get(
+  '/login',
+  (req, res) => {
+    getUsers()
+        .then(users => {
+          const {login, password} = req.query;
+          const user = users.find(user => user.username === login);
+          if (!user) {
+            res.status(404).end();
+            return;
+          }
+          if (user.password !== password) {
+            res.status(401).end();
+            return;
+          }
+          res.status(200).end();
+        });
+  },
+);
 
 app.listen(8002, function () {
   console.log('Example app listening on port 8002!');
