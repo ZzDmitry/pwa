67be4343cc77e12370508f732b5336372586dfc0
dmitry <dmitry.zyryanov@execution.su>
2019-03-05 16:23:54 +0700

    service worker linted

--- login-app/app/sw.js
+++ login-app/app/sw.js
@@ -1,51 +1,33 @@
-/*
- *
- *  Air Horner
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
+/* global caches fetch */
 
-const version = "0.6.11";
+// eslint-disable-next-line no-undef,no-restricted-globals
+const swSelf = self;
+
+const version = '0.6.11';
 const cacheName = `airhorner-${version}`;
-self.addEventListener('install', e => {
+swSelf.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `/`,
-        `/index.html`,
-        `/styles/main.css`,
-        `/scripts/main.js`,
-        `/scripts/comlink.global.js`,
-        `/scripts/messagechanneladapter.global.js`,
-        `/scripts/pwacompat.min.js`,
-      ])
-          .then(() => self.skipWaiting());
-    })
+    caches.open(cacheName).then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/styles/main.css',
+      '/scripts/main.js',
+      '/scripts/comlink.global.js',
+      '/scripts/messagechanneladapter.global.js',
+      '/scripts/pwacompat.min.js',
+    ])
+      .then(() => swSelf.skipWaiting())),
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+swSelf.addEventListener('activate', (event) => {
+  event.waitUntil(swSelf.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
+swSelf.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => response || fetch(event.request)),
   );
 });
