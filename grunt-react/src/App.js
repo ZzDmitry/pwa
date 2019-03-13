0dfb4421368e0ba88d121b66d01ffb459b094d35
dmitry <dmitry.zyryanov@execution.su>
2019-03-13 16:07:52 +0700

    sample react app added

---
+++ grunt-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <h1>
+        React application
+      </h1>
+    );
+  }
+}
+
+export default App;
